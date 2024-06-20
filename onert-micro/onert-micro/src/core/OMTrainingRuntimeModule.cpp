a1c498b623308495e788e41827dd7dabb4fc5d57
Chunseok Lee <chunseok.lee@samsung.com>
2024-06-20 16:17:57 +0900

    aux funcs in OMTrainingInterpreter
    - run(), allocateInputs(), getInputData, getInputDataAt, getOutputDataAt
    
    Signed-off-by: Chunseok Lee <chunseok.lee@samsung.com>

--- onert-micro/onert-micro/src/core/OMTrainingRuntimeModule.cpp
+++ onert-micro/onert-micro/src/core/OMTrainingRuntimeModule.cpp
@@ -302,3 +302,8 @@ OMStatus OMTrainingRuntimeModule::loadCheckpointData(OMConfig &config, const cha
 
   return status;
 }
+
+void* getInputData(int32_t index)
+{
+  return _training_handler.getInputData(index);
+}
